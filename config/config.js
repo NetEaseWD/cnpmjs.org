8bdf56701c54b77535944252b34c25802ceffe14
hzxiejin <hzxiejin@corp.netease.com>
2015-10-08 10:21:33 +0800

    add config.js

---
+++ config/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	debug: false,
+  scopes: ['@netease'],
+  enableCluster: true, // enable cluster mode
+  mysqlServers: [
+    {
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'root',
+    }
+  ],
+  mysqlDatabase: 'cnpm',
+  enablePrivate: true, // enable private mode
+  admins: {
+    june01: 'june_01@163.com',
+  },
+  syncModel: 'exist',// 'none', 'all', 'exist'
+  sourceNpmRegistry: 'http://registry.npmjs.org',
+  sourceNpmRegistryIsCNpm: false,
+  database: {
+	  db: 'cnpm',
+	  username: 'root',
+	  password: '', 
+	  dialect: 'mysql',
+	  host: '127.0.0.1',
+	  port: 3306
+	}
+}
